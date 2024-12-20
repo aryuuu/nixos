fa311a37185f6e6e7447eaf6627637601c6ce83f
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2024-12-20 11:02:09 +0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "my nixos system flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.11";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let 
+      system = "x86_64-linux";
+      
+      pkgs = import nixpkgs {
+        inherit system;
+        
+        config = {
+            allowUnfree = true;
+        };
+      };
+
+    in 
+    {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit system; };
+
+          modules = [
+            ./nixos/configuration.nix
+          ];
+        };
+      };
+    };
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,224 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Jakarta";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.blueman.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+
+    #media-session.enable = true;
+  };
+
+  virtualisation.docker = {
+    enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+  users.users.openvpn = {
+    isSystemUser = pkgs.lib.mkForce true;
+    description = "OpenVPN User";
+
+    group = "openvpn";
+  };
+  users.groups.openvpn = {};
+  services.dbus.packages = [ pkgs.openvpn3 ];
+  systemd.tmpfiles.rules = [
+    "d /etc/openvpn3/configs 0755 root root -"
+  ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.fatt = {
+    isNormalUser = true;
+    description = "fatt";
+    extraGroups = [ "networkmanager" "wheel" "docker" "video" ];
+    packages = with pkgs; [
+      kate
+      alacritty
+      fzf
+      slack
+      postman
+      go
+      zig
+      nodejs
+      brave
+      pass-wayland
+      kubectl
+      k9s
+      terraform
+      delta
+      telegram-desktop
+      dbeaver-bin
+      robo3t
+      btop
+      fastfetch
+      zed-editor
+      discord
+      jq
+      yq
+      ripgrep
+      nerdfonts
+      font-awesome
+      waybar
+      swaybg
+      swaylock-fancy
+      rofi-wayland
+      networkmanager_dmenu
+      dmenu-wayland
+      dunst
+      libnotify
+      zathura
+      cliphist
+      wl-clipboard
+      file
+      bluez
+      wf-recorder
+      gimp
+      drawing
+      grim
+      slurp
+      swappy
+      tesseract
+      python3
+      cargo
+      ranger
+      lf
+      aws-vault
+      chamber
+      bat
+      pulseaudio
+      pavucontrol
+      awscli2
+    ];
+
+    shell = pkgs.fish;
+  };
+  programs.fish.enable = true;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+  programs.steam = {
+     enable = true;
+  };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    neovim
+    git
+    stow
+    tmux
+    gnupg
+    gnumake
+    d2coding
+    gcc
+    unzip
+    ponymix
+    openvpn3
+    psmisc
+    brightnessctl
+  ];
+
+  environment.sessionVariables = {
+     EDITOR = "nvim";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
