d9a0170032193dada323d065e98751b148f50ca9
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2026-04-22 10:19:07 +0700

    chore: update nixpkgs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -52,6 +52,7 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.blueman.enable = true;
+  services.gnome.gnome-keyring.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
