f3ff8d29269569bfbe67293f92cbe8a55c4f67a8
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2026-05-25 15:19:04 +0700

    feat: polkit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -172,6 +172,7 @@
       discord
       jq
       yq-go
+      gum
       ripgrep
       nerd-fonts.droid-sans-mono
       nerd-fonts.jetbrains-mono
@@ -242,6 +243,7 @@
       comma
       hurl
       net-tools
+      tree-sitter
 
       amazon-q-cli
       claude-code
@@ -277,6 +279,21 @@
         "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
         "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
       };
+    };
+  };
+  security.polkit.enable = true;
+
+  systemd.user.services.polkit-kde-agent = {
+    description = "PolicyKit Authentication Agent";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
     };
   };
 
